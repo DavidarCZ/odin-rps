ceabd48a396a46b0fc466ca1635bd57ad239b6d4
DavidarCZ <94920054+DavidarCZ@users.noreply.github.com>
2025-03-08 21:49:38 +0100

    Implement getComputerChoice function
    Implemented function to randomly generate computer
    choice between rock, paper, and scissors.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+        default:
+            return 'rock';
+    }
+}
+
+console.log(getComputerChoice());
