745ca41f487d791b04baca7119395f4c41a405a6
DavidarCZ <94920054+DavidarCZ@users.noreply.github.com>
2025-03-08 22:22:39 +0100

    Implement playRound function
    Implemented function that plays a single round of
    the game. It gets the player's choice and the
    computer's choice, then determines the winner and
    updates the score accordingly.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let humanScore = 0;
+let computerScore = 0;
+
 // generate computer choice
 function getComputerChoice() {
-    switch (Math.floor(Math.random() * 3)) {
+    switch (Math.floor(Math.random() * 3)) 
+    {
         case 0:
             return 'rock';
         case 1:
@@ -11,15 +15,44 @@ function getComputerChoice() {
             return 'rock';
     }
 }
-// 
+
+//prompt user for choice
 function getHumanChoice() {
     let choice = prompt("Rock, paper, scissors?").toLowerCase();
-    if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
+    if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') 
+    {
         alert("Are you sure you know how to play this game?");
         return null;
     }
     return choice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+//play a round of rps
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+    
+    if (humanChoice === null) 
+    {
+        return;
+    }
+
+    if (humanChoice === computerChoice) 
+    {
+      console.log("It's a tie!");
+    } 
+    else if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'scissors' && computerChoice === 'paper')) 
+    {
+        console.log("You win!");
+        humanScore++;
+    } 
+    else 
+    {
+        console.log("You lose!");
+        computerScore++;
+    }
+}
+
+
+
+playRound();
